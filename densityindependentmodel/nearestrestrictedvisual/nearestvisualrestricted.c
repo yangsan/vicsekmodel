fd4303b03cdb771de522c46df19156960edb360d
Kevin Young <kevinyounglogin@163.com>
2014-03-17 12:34:12 +0800

    写了限定视角x最近邻个体的代码，结果似乎和最近邻模型没有什么本质的区别，代码似乎也有问

--- densityindependentmodel/nearestrestrictedvisual/nearestvisualrestricted.c
+++ densityindependentmodel/nearestrestrictedvisual/nearestvisualrestricted.c
@@ -3,17 +3,18 @@
 #include <math.h>
 
 #define N 100
-#define L 15
+#define L 5
 #define R 1.0
 #define V 0.03
-#define ETA 1.0
+#define ETA 0.5
 #define STEP 10000
 #define NC 6
+#define PI 3.14159265358979323846
 
 #define random() rand()/(RAND_MAX+1.0)
 
-int simulation(double xcor[], double ycor[], double xdir[], double ydir[]);
-double distance(double x1, double y1, double x2, double y2);
+int simulation(double xcor[], double ycor[], double xdir[], double ydir[], double omega);
+double distance(double x, double y);
 
 int main(int argc, char *argv[])
 {
@@ -24,6 +25,7 @@ int main(int argc, char *argv[])
     /*iterator*/
     double denominator;
     /*denominator*/
+    double omega;
     FILE *fp;
     char filename[100];
 /*end of decleration*/
@@ -31,6 +33,8 @@ int main(int argc, char *argv[])
 /*begin of initializing*/
     srand((unsigned int)time(NULL));
     /*initializing the random seed*/
+
+    omega = cos((0./3.)*PI/2.);
 
     for(i=0; i<N; i++)
     {
@@ -55,7 +59,7 @@ int main(int argc, char *argv[])
             ydir[i] = -random();
         }
 
-        denominator = distance(xdir[i], ydir[i],0.0, 0.0);
+        denominator = distance(xdir[i], ydir[i]);
         xdir[i] /= denominator;
         ydir[i] /= denominator;
         /*renormalizaiton*/
@@ -65,7 +69,7 @@ int main(int argc, char *argv[])
 /*begin of simulation*/
     for(k=0; k<STEP; k++)
     {
-        simulation(xcor, ycor, xdir, ydir);
+        simulation(xcor, ycor, xdir, ydir, omega);
         sprintf(filename, "./data/%i.out", k);
         fp = fopen(filename,"w");
         for(i = 0;i<N;i++)
@@ -90,7 +94,7 @@ int main(int argc, char *argv[])
  * =====================================================================================
  */
 
-int simulation(double xcor[], double ycor[], double xdir[], double ydir[]){
+int simulation(double xcor[], double ycor[], double xdir[], double ydir[], double omega){
 
     double fordistance[N * 9];
     int forlabel[N * 9];
@@ -105,6 +109,8 @@ int simulation(double xcor[], double ycor[], double xdir[], double ydir[]){
     /*the sum of neighbors' directions*/
     double x, y;
     /*temps for perturbation*/
+    double dx, dy;
+    double dis;
     double theta;
     /*for perturbation*/
  
@@ -136,32 +142,136 @@ int simulation(double xcor[], double ycor[], double xdir[], double ydir[]){
         sumy = 0;
         for(j=0; j<N; j++)
         {
-            fordistance[j] = distance(xcor[i], ycor[i], xcor[j], ycor[j]);
-            forlabel[j] = j;
+            dx = xcor[j] - xcor[i];
+            dy = ycor[j] - ycor[i];
+            dis = distance(dx, dy);
+            if((dis==0 || dx*xdir[i] + dy*ydir[i])/dis > omega){
+                fordistance[j]  = dis;
+                forlabel[j] = j;
+            }
+            else{
+                fordistance[j] = 5*L;
+                forlabel[j] = -1;
+            }
 
-            fordistance[j + N] = distance(xcor[i], ycor[i], xcor[j]-L, ycor[j]+L);
-            forlabel[j + N] = j;
+            /*fordistance[j + N] = distance(xcor[i], ycor[i], xcor[j]-L, ycor[j]+L);*/
+            /*forlabel[j + N] = j;*/
+            dx = xcor[j] - L - xcor[i];
+            dy = ycor[j] + L - ycor[i];
+            dis = distance(dx, dy);
+            if((dis==0 || dx*xdir[i] + dy*ydir[i])/dis > omega){
+                fordistance[j + N]  = dis;
+                forlabel[j + N] = j;
+            }
+            else{
+                fordistance[j+ N] = 5*L;
+                forlabel[j+ N] = -1;
+            }
 
-            fordistance[j + 2*N] = distance(xcor[i], ycor[i], xcor[j], ycor[j]+L);
-            forlabel[j + 2*N] = j;
+            /*fordistance[j + 2*N] = distance(xcor[i], ycor[i], xcor[j], ycor[j]+L);*/
+            /*forlabel[j + 2*N] = j;*/
 
-            fordistance[j + 3*N] = distance(xcor[i], ycor[i], xcor[j]+L, ycor[j]+L);
-            forlabel[j + 3*N] = j;
+            dx = xcor[j] - xcor[i];
+            dy = ycor[j] + L - ycor[i];
+            dis = distance(dx, dy);
+            if((dis==0 || dx*xdir[i] + dy*ydir[i])/dis > omega){
+                fordistance[j + 2*N]  = dis;
+                forlabel[j + 2*N] = j;
+            }
+            else{
+                fordistance[j+ 2*N] = 5*L;
+                forlabel[j+ 2*N] = -1;
+            }
 
-            fordistance[j + 4*N] = distance(xcor[i], ycor[i], xcor[j]-L, ycor[j]);
-            forlabel[j + 4*N] = j;
+            /*fordistance[j + 3*N] = distance(xcor[i], ycor[i], xcor[j]+L, ycor[j]+L);*/
+            /*forlabel[j + 3*N] = j;*/
 
-            fordistance[j + 5*N] = distance(xcor[i], ycor[i], xcor[j]+L, ycor[j]);
-            forlabel[j + 5*N] = j;
+            dx = xcor[j] + L - xcor[i];
+            dy = ycor[j] + L - ycor[i];
+            dis = distance(dx, dy);
+            if((dis==0 || dx*xdir[i] + dy*ydir[i])/dis > omega){
+                fordistance[j + 3*N]  = dis;
+                forlabel[j + 3*N] = j;
+            }
+            else{
+                fordistance[j + 3*N] = 5*L;
+                forlabel[j + 3*N] = -1;
+            }
 
-            fordistance[j + 6*N] = distance(xcor[i], ycor[i], xcor[j]- L, ycor[j] - L);
-            forlabel[j + 6*N] = j;
+            /*fordistance[j + 4*N] = distance(xcor[i], ycor[i], xcor[j]-L, ycor[j]);*/
+            /*forlabel[j + 4*N] = j;*/
 
-            fordistance[j + 7*N] = distance(xcor[i], ycor[i], xcor[j], ycor[j] - L);
-            forlabel[j + 7*N] = j;
+            dx = xcor[j] - xcor[i];
+            dy = ycor[j] - L - ycor[i];
+            dis = distance(dx, dy);
+            if((dis==0 || dx*xdir[i] + dy*ydir[i])/dis > omega){
+                fordistance[j + 4*N]  = dis;
+                forlabel[j + 4*N] = j;
+            }
+            else{
+                fordistance[j + 4*N] = 5*L;
+                forlabel[j + 4*N] = -1;
+            }
 
-            fordistance[j + 8*N] = distance(xcor[i], ycor[i], xcor[j] + L, ycor[j] - L);
-            forlabel[j + 8*N] = j;
+            /*fordistance[j + 5*N] = distance(xcor[i], ycor[i], xcor[j]+L, ycor[j]);*/
+            /*forlabel[j + 5*N] = j;*/
+
+            dx = xcor[j] + L - xcor[i];
+            dy = ycor[j] - ycor[i];
+            dis = distance(dx, dy);
+            if((dis==0 || dx*xdir[i] + dy*ydir[i])/dis > omega){
+                fordistance[j + 5*N]  = dis;
+                forlabel[j + 5*N] = j;
+            }
+            else{
+                fordistance[j + 5*N] = 5*L;
+                forlabel[j + 5*N] = -1;
+            }
+
+            /*fordistance[j + 6*N] = distance(xcor[i], ycor[i], xcor[j]- L, ycor[j] - L);*/
+            /*forlabel[j + 6*N] = j;*/
+
+            dx = xcor[j] - L - xcor[i];
+            dy = ycor[j] - L - ycor[i];
+            dis = distance(dx, dy);
+            if((dis==0 || dx*xdir[i] + dy*ydir[i])/dis > omega){
+                fordistance[j + 6*N]  = dis;
+                forlabel[j + 6*N] = j;
+            }
+            else{
+                fordistance[j + 6*N] = 5*L;
+                forlabel[j + 6*N] = -1;
+            }
+
+            /*fordistance[j + 7*N] = distance(xcor[i], ycor[i], xcor[j], ycor[j] - L);*/
+            /*forlabel[j + 7*N] = j;*/
+
+            dx = xcor[j] - xcor[i];
+            dy = ycor[j] - L - ycor[i];
+            dis = distance(dx, dy);
+            if((dis==0 || dx*xdir[i] + dy*ydir[i])/dis > omega){
+                fordistance[j + 7*N]  = dis;
+                forlabel[j + 7*N] = j;
+            }
+            else{
+                fordistance[j + 7*N] = 5*L;
+                forlabel[j + 7*N] = -1;
+            }
+
+            /*fordistance[j + 8*N] = distance(xcor[i], ycor[i], xcor[j] + L, ycor[j] - L);*/
+            /*forlabel[j + 8*N] = j;*/
+
+            dx = xcor[j] + L - xcor[i];
+            dy = ycor[j] - L - ycor[i];
+            dis = distance(dx, dy);
+            if((dis==0 || dx*xdir[i] + dy*ydir[i])/dis > omega){
+                fordistance[j + 8*N]  = dis;
+                forlabel[j + 8*N] = j;
+            }
+            else{
+                fordistance[j + 8*N] = 5*L;
+                forlabel[j + 8*N] = -1;
+            }
         }
 
         for(j=0 ; j< NC; j++){
@@ -176,14 +286,16 @@ int simulation(double xcor[], double ycor[], double xdir[], double ydir[]){
                    forlabel[k] = inttemp;
                 }
             }
-            sumx += xdir[forlabel[j]];
-            sumy += ydir[forlabel[j]];
+            if(forlabel[j] != -1){
+                sumx += xdir[forlabel[j]];
+                sumy += ydir[forlabel[j]];
+            }
         }
 
 
         xdirt[i] = sumx;
         ydirt[i] = sumy;
-        denominator = distance(xdirt[i], ydirt[i],0.0,0.0);
+        denominator = distance(xdirt[i], ydirt[i]);
         xdirt[i] /= denominator;
         ydirt[i] /= denominator;
         /*end of direction*/
@@ -218,7 +330,7 @@ int simulation(double xcor[], double ycor[], double xdir[], double ydir[]){
  *  Description:  return the distance of the two points passed in
  * =====================================================================================
  */
-double distance(double x1, double y1, double x2, double y2)
+double distance(double x, double y)
 {
-    return sqrt(pow(x1 - x2,2)+pow(y1 - y2,2));
+    return sqrt(pow(x,2)+pow(y,2));
 }
